d274f15bce42794da905a9fb8c1be0426bf13676
Rakesh <149894070+rakeshdeep@users.noreply.github.com>
2024-02-17 12:52:28 +0530

    day 3 |  adding Fetch API and changing data dynamically level 1

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,23 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 export default function Question() {
-    
+    const [data, setData] = useState([])
+    const [currentIndex, setCurrentIndex] = useState(0)
+
+
+    useEffect(()=>{
+        fetch('./quiz.json')
+            .then(response => response.json())
+            .then(data =>{
+                setData(data.data)
+                console.table(data.data)
+            }).catch(error => console.error("getting some "+error));
+    },[]);
+
+    const handleNext = () => {
+        setCurrentIndex((prevIndex) => (prevIndex + 1 ) % data.length);
+    };
 
     return (
-
         <>
-            
-
             <div className='question-container'>
-                <p className={"question"}>What is the capital Of India ?</p>
-                <p className={`options`}>Korea</p>
-                <p className={`options`}>taliban</p>
-                <p className={`options`}>Pakistan</p>
-                <p className={`options correct`}>India</p>
+                <p className={"question"}> {data[currentIndex]?.question}</p>
+                <p className={`options`}>{data[currentIndex]?.opt1}</p>
+                <p className={`options`}>{data[currentIndex]?.opt2}</p>
+                <p className={`options`}>{data[currentIndex]?.opt3}</p>
+                <p className={`options correct`}>{data[currentIndex]?.opt4}</p>
                 <div className="special-btn">
                     <button className="btns pre">preview</button>
                     <button className="btns submit">submit</button>
-                    <button className="btns next">next</button>
+                    <button className="btns next" onClick={handleNext}>next</button>
                 </div>
             </div>
         </>
